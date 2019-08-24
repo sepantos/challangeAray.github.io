51e6fcb42876787a3b1bd8db7ed7d3ce0b6ef77e
sepantos <septiadiandrianto8@gmail.coclear>
2019-08-24 08:01:06 +0700

    tugas array

---
+++ array.js
@@ -0,0 +1,7 @@
+var bumbudapur = ['jengkol','garam','santan','penyedap'];
+
+bumbudapur.splice(2, 0,'kecap');
+bumbudapur.splice(4,0,'miyakgoreng');
+bumbudapur.splice(1, 1, 'micin');
+console.log(bumbudapur)
+console.log("index ke-3: " + bumbudapur[3])
